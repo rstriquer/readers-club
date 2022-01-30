b82e68db3d9c6bd7441bc551b9edba33f828cdbc
Ricardo Soares <rstriquer@gmail.com>
2022-01-30 16:14:14 -0300

    restore bootstrap link to Inertia interface

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,8 @@ import { render } from 'react-dom'
 import { createInertiaApp } from '@inertiajs/inertia-react'
 import { InertiaProgress } from '@inertiajs/progress'
 
+require('./bootstrap');
+
 InertiaProgress.init()
 createInertiaApp({
   resolve: name => require(`./components/${name}/index.js`),
