340b2ccc78d1bb8e264dccdfb919ccb9cc5efe79
Ricardo Soares <rstriquer@gmail.com>
2022-01-30 16:10:17 -0300

    Eliminate not needed controller
    - Delete autobuilt file for base controller

--- app/Http/Controllers/HomeIndexPublicController.php
+++ app/Http/Controllers/HomeIndexPublicController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Inertia\Controller;
 use Inertia\Inertia;
+use Inertia\Response;
 
 class HomeIndexPublicController extends Controller
 {
-    public function __invoke(Request $request)
+    public function __invoke(Request $request) : Response
     {
         return Inertia::render('HomeIndexPublic', []);
     }
