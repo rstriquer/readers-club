609d6da732956de55c71ee5679b07968fde6b13e
Ricardo Soares <rstriquer@gmail.com>
2022-02-01 02:01:11 -0300

    add Form::rating and delete unecessary methods
    - implement changes on AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,27 +2,18 @@
 
 namespace App\Providers;
 
+use Collective\Html\FormFacade AS Form;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
-
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
     public function boot()
     {
-        //
+        Form::macro('rating', function(string $label, int $userRanking = 0)
+        {
+            return '<div id="' . $label
+                . '"></div><input type=hidden id="ratings-value" value="'
+                . $userRanking . '" />';
+        });
     }
 }
