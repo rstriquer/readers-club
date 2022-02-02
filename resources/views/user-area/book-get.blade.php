2783eed1b2b57968c3b45e0191418caa3094cd19
Ricardo Soares <rstriquer@gmail.com>
2022-02-02 00:31:58 -0300

    Fix some template fields on get

--- resources/views/user-area/book-get.blade.php
+++ resources/views/user-area/book-get.blade.php
@@ -7,18 +7,18 @@ $title = 'Adding new book to my library';
 @section('form')
     <div class="form-group row">
         <div class="col-md-4">
-            <img src="{{ $book['thumbnail'] }}">
+            <img src="{{ $userBook->book->thumbnail }}">
         </div>
         <div class="col-md-8">
-            <h1 class="fw-bold">{{ $book['title'] }}</h1>
-            <h5 class="fw-bold">{{ __('Authors') }}: {{ $book['authors'] }}</h5>
-            <h5 class="fw-light">{{ __('ISBN') }}: {{ $book['isbn'] }}</h5>
-            <p class="fw-light">{{ __('Publisher') }}: {{ $book['publisher'] }}</p>
-            <p class="fw-light">{{ __('Published') }}: {{ $book['published'] }}</p>
-            <p class="fw-light">{{ __('Page count') }}: {{ $book['pageCount'] }}</p>
+            <h1 class="fw-bold">{{ $userBook->book->title }}</h1>
+            <h5 class="fw-bold">{{ __('Authors') }}: {{ $userBook->book->presentAuthors() }}</h5>
+            <h5 class="fw-light">{{ __('ISBN') }}: {{ $userBook->book->isbn }}</h5>
+            <p class="fw-light">{{ __('Publisher') }}: {{ $userBook->book->publisher?->name }}</p>
+            <p class="fw-light">{{ __('Published') }}: {{ $userBook->book->published?->format('d/m/Y') }}</p>
+            <p class="fw-light">{{ __('Page count') }}: {{ $userBook->book->pageCount }}</p>
 
             {{ Form::label(null, __('Your ratings'), ['class' => 'control-label']) }}
-            {!! Form::rating('ratings', $userBook->stars ?? 0) !!}<br />
+            {!! Form::rating('ratings', data_get($userBook, 'rating', 0)) !!}<br />
 
             {{ Form::label('reading_from', __('Reading since'), ['class' => 'control-label']) }}
             {{ Form::date('reading_from', data_get($userBook, 'reading_from'), ['class' => 'form-control']) }} <br />
@@ -29,7 +29,7 @@ $title = 'Adding new book to my library';
             {{ Form::label('review', __('Write a review'), ['class' => 'control-label']) }}
             {{ Form::TextArea('review', data_get($userBook, 'review'), ['class' => 'form-control']) }} <br />
 
-            <a target="_blank" href="{{ $book['previewLink'] }}" class="btn btn-light">Preview</a>
+            <a target="_blank" href="{{ $userBook->book->previewLink }}" class="btn btn-light">Preview</a>
             {{ Form::submit(__('Save book'), ['class'=>'btn btn-primary']) }}
         </div>
     </div>
