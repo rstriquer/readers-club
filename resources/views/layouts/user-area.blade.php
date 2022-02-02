1f15ee3e1dfc99455e7227fe6b67ce15563bf41f
Ricardo Soares <rstriquer@gmail.com>
2022-02-02 01:34:25 -0300

    add template message display area

--- resources/views/layouts/user-area.blade.php
+++ resources/views/layouts/user-area.blade.php
@@ -7,6 +7,33 @@
             <div class="card">
                 <div class="card-header">{{ __($title) }}</div>
                 <div class="card-body">
+
+                    @if(Session::has('success'))
+                    <div class="alert alert-success alert-dismissible" role="alert">
+                        <strong>{{ __('Success!') }}</strong> {!! Session::get('success') !!}
+                    </div>
+                    @endif
+                    @if(Session::has('error') || Session::has('errors'))
+                    <div class="alert alert-danger alert-dismissible" role="alert">
+                        <strong>{{ __('Atention!') }}</strong>
+                        <p>
+                            @foreach(Session::get('errors')->all() + [Session::get('error')] as $error)
+                            &bull; {!!$error!!} <br>
+                            @endforeach
+                        </p>
+                    </div>
+                    @endif
+                    @if(Session::has('info'))
+                    <div class="alert alert-info alert-dismissible" role="alert">
+                        <strong>{{ __('Atention!') }}</strong>
+                        <p>
+                            @foreach(Session::get('info') as $info)
+                            &bull; {!!$info!!} <br>
+                            @endforeach
+                        </p>
+                    </div>
+                    @endif
+
                     {{ Form::open($form) }}
                     {{ Form::token() }}
                     @yield('form')
