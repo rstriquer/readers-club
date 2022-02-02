b0fbce723c03e5a51ff4eea17600b2b994f287af
Ricardo Soares <rstriquer@gmail.com>
2022-02-02 01:54:49 -0300

    Add POST::/user-area/{isbn}
    - Add route group to /user-area path
    - Add post route to group

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,19 @@
 <?php
 
 use App\Http\Controllers\BookGetUserAreaController;
+use App\Http\Controllers\BookStoreUserAreaController;
 use App\Http\Controllers\HomeIndexPublicController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomeIndexPublicController::class)->name('public.index');
-Route::get('/user-area/{isbn}', BookGetUserAreaController::class)
-    ->name('user-area.get.isbn')
-    ->middleware(['auth']);
+Route::name('user-area.')
+    ->middleware(['auth'])
+    ->prefix('user-area')
+    ->group(function() {
+        Route::get('{isbn}', BookGetUserAreaController::class)->name('get.isbn');
+        Route::post('{isbn}', BookStoreUserAreaController::class)->name('post.isbn');
+    });
 
 // Route::get('/user-area' // user-area.list list book
 // Route::delete('/user-area/{isbn}', BookDeleteUserAreaController::class) // delete book
 // Route::get('/user-area/{isbn}' // add or edit user book (if does not exists show empty form)
-// Route::post('/user-area/{isbn}',  // store book
