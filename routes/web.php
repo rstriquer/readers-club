061ec9d96bc01cb4f3e84d55d80211c321b603aa
Ricardo Soares <rstriquer@gmail.com>
2022-02-02 02:27:13 -0300

    add books list to route file
    - add BookListUserAreaController class

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\BookGetUserAreaController;
+use App\Http\Controllers\BookListUserAreaController;
 use App\Http\Controllers\BookStoreUserAreaController;
 use App\Http\Controllers\HomeIndexPublicController;
 use Illuminate\Support\Facades\Route;
@@ -10,10 +11,9 @@ Route::name('user-area.')
     ->middleware(['auth'])
     ->prefix('user-area')
     ->group(function() {
+        Route::get('/', BookListUserAreaController::class)->name('list');
         Route::get('{isbn}', BookGetUserAreaController::class)->name('get.isbn');
         Route::post('{isbn}', BookStoreUserAreaController::class)->name('post.isbn');
     });
 
-// Route::get('/user-area' // user-area.list list book
 // Route::delete('/user-area/{isbn}', BookDeleteUserAreaController::class) // delete book
-// Route::get('/user-area/{isbn}' // add or edit user book (if does not exists show empty form)
