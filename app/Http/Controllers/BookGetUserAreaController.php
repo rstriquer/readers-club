5d140a82598c569f08e2e73105f335d6082fad4b
Ricardo Soares <rstriquer@gmail.com>
2022-02-02 00:31:44 -0300

    Shorten the BookGetUserAreaController code using services

--- app/Http/Controllers/BookGetUserAreaController.php
+++ app/Http/Controllers/BookGetUserAreaController.php
@@ -17,44 +17,13 @@ class BookGetUserAreaController extends UserAreaController
     {
         parent::__invoke($isbn);
         try {
-            $userBook = UserBook::find($isbn);
+            $userBook = $this->service->getUserBook($isbn);
             if ( ! $userBook ) {
-                $book = Cache::get('user-area.get.isbn.' . $isbn);
-                if (true || $book === null) {
-                    try {
-                        $result
-                            = app(
-                                Client::class,
-                                ['config' => ['base_uri' => 'https://www.googleapis.com']]
-                            )
-                            ->request('GET', '/books/v1/volumes?q=isbn:' . $isbn)
-                            ->getBody()
-                            ->getContents();
-                        $result = data_get(json_decode($result, true), 'items.0');
-                        $book = [
-                            'title' => data_get($result, 'volumeInfo.title'),
-                            'authors' => implode(', ', data_get($result, 'volumeInfo.authors')),
-                            'thumbnail' => data_get($result, 'volumeInfo.imageLinks.thumbnail'),
-                            'previewLink' => data_get($result, 'volumeInfo.previewLink'),
-                            'publisher' => data_get($result, 'volumeInfo.publisher'),
-                            'published' => data_get($result, 'volumeInfo.publishedDate'),
-                            'pageCount' => data_get($result, 'volumeInfo.pageCount'),
-                            'isbn' => $isbn,
-                            'id_api' => data_get($result, 'id'),
-                        ];
-                        Cache::put(
-                            'user-area.get.isbn.' . $isbn,
-                            $book,
-                            now()->addDay()
-                        );
-                    } catch(\Exception $err) {
-                        throw $err;
-                    }
-                }
+                $book = $this->service->getBook($isbn);
                 $userBook = app(UserBook::class);
+                $userBook->book()->associate($book);
             }
             return view('user-area.book-get', [
-                'book' => $book,
                 'userBook' => $userBook,
                 'form' => [ 'url' => '/' ],
             ]);
